2d1d7732c56f28847612d2dc088fc931b1797d05
deepshah9696 <deepshah512@gmail.com>
2024-01-21 11:47:54 +0530

    app js updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
+/** @format */
+
 import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
+import HomePage from './Pages/HomePage.tsx';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import HomePageContents from './Pages/HomePageContents';
+import { createWeb3Modal, defaultWagmiConfig } from '@web3modal/wagmi/react';
+import { polygonZkEvmTestnet, gnosisChiado, neonDevnet, goerli , optimismGoerli, scrollSepolia, arbitrumSepolia, celoAlfajores,lineaTestnet,mantleTestnet} from 'wagmi/chains';
+import Create from './Pages/Preferences';
+import SendAssets from './Pages/SendAssets';
+import sendNFTs from './Pages/SendAssets';
+import { WagmiConfig, createConfig } from 'wagmi';
+import {
+  ConnectKitProvider,
+  ConnectKitButton,
+  getDefaultConfig,
+} from 'connectkit';
 
+const config = createConfig(
+  getDefaultConfig({
+    // Required API Keys
+    alchemyId: '2MCR81xh4vPrqI0y4SQtv09NTnb', // or infuraId
+    walletConnectProjectId: '12a12e8b3daeab1b03edb985fb2da046',
+
+    // Required
+    appName: 'WalletSync',
+  })
+);
+// https://youtube.com/shorts/ESfDVsjJKtM?feature=share
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WagmiConfig config={config}>
+      <ConnectKitProvider>
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={<HomePage pageContents={HomePageContents} />}
+            />
+            <Route
+              path="/preferences"
+              element={<HomePage pageContents={Create} />}
+            />
+            <Route
+              path="/assets"
+              element={<HomePage pageContents={sendNFTs} />}
+            />
+          </Routes>
+        </Router>
+      </ConnectKitProvider>
+    </WagmiConfig>
   );
 }
 
